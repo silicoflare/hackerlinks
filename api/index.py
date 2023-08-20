2a5d1fec119abaf8e0c5d9c09955f0012b871506
SilicoFlare <SilicoFlare@users.noreply.github.com>
2023-08-20 13:05:06 +0000

    Generated website files
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/index.py
+++ api/index.py
@@ -12,38 +12,78 @@ def index():
     with open('data/index.json', 'r') as data:
         components = json.loads(data.read())['components']
     return render_template('main.html', 
-    project_name="Sample",
-    main_heading="",
-    content="",
-    stylesheets=['', ''],
-    body_classes="",
-    heading_classes="",
-    content_classes="",
-    subhead_classes="",
+    project_name="HackerSpace",
+    main_heading="&lt;HackerSpace&gt;",
+    content="Development Club of PES University",
+    stylesheets=["<style>@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Poppins&display=swap');</style>", ''],
+    body_classes="bg-hsp font-pop",
+    heading_classes="font-hsp text-white",
+    content_classes="text-white",
+    subhead_classes="font-hsp text-white",
     components=components
     )
 
-@app.route('/newpage')
-def newpage():
-    with open('data/newpage.json', 'r') as data:
+@app.route('/rr')
+def rr():
+    with open('data/rr-campus.json', 'r') as data:
         components = json.loads(data.read())['components']
     return render_template('main.html', 
-    project_name="Sample",
-    main_heading="",
-    content="",
-    stylesheets=['', ''],
-    body_classes="",
-    heading_classes="",
-    content_classes="",
-    subhead_classes="",
+    project_name="HackerSpace",
+    main_heading="&lt;HackerSpace&gt;",
+    content="Development Club of PES University",
+    stylesheets=["<style>@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Poppins&display=swap');</style>", ''],
+    body_classes="bg-hsp font-pop",
+    heading_classes="font-hsp text-white",
+    content_classes="text-white",
+    subhead_classes="font-hsp text-white",
     components=components
     )
 
-@app.route('/blah')
-def blah():
-    return redirect('somelink.com')
+@app.route('/ec')
+def ec():
+    with open('data/ec-campus.json', 'r') as data:
+        components = json.loads(data.read())['components']
+    return render_template('main.html', 
+    project_name="HackerSpace",
+    main_heading="&lt;HackerSpace&gt;",
+    content="Development Club of PES University",
+    stylesheets=["<style>@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Poppins&display=swap');</style>", ''],
+    body_classes="bg-hsp font-pop",
+    heading_classes="font-hsp text-white",
+    content_classes="text-white",
+    subhead_classes="font-hsp text-white",
+    components=components
+    )
 
-@app.route('/random')
-def random():
-    return redirect('someotherlink.com')
+@app.route('/discord')
+def discord():
+    return redirect('https://discord.gg/9m7ad5mDVK')
+
+@app.route('/github')
+def github():
+    return redirect('https://bit.ly/hackerspace-github')
+
+@app.route('/twitter')
+def twitter():
+    return redirect('http://bit.ly/hackerspace-twitter-rr')
+
+@app.route('/linkedin')
+def linkedin():
+    return redirect('https://bit.ly/hackerspace-linkedin')
+
+@app.route('/rr/instagram')
+def rr_instagram():
+    return redirect('http://bit.ly/hackerspace-instagram')
+
+@app.route('/ec/instagram')
+def ec_instagram():
+    return redirect('http://bit.ly/hackerspace-instagram-ecc')
+
+@app.route('/rr/youtube')
+def rr_youtube():
+    return redirect('http://bit.ly/hackerspace-youtube')
+
+@app.route('/ec/youtube')
+def ec_youtube():
+    return redirect('http://bit.ly/hackerspace-youtube-ecc')
 
